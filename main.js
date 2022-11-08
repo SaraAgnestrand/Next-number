45f89a8aa8f76937856eb8ec5b66b6afdf8a1449
Sara Agnestrand <sara@agnestrand.net>
2022-11-08 14:34:25 +0100

    adding new code

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const inputNr=document.querySelector(".inputNr");
+const button=document.querySelector("button");
+const h3=document.querySelector("h3");
+
+button.addEventListener("click", function() {
+    console.log("Innan funktionsanrop")
+    const nextNumber = getNextNumber();
+    h3.innerText = "Nästa nummer är " + nextNumber + ".";
+    
+});
+
+
+function getNextNumber(){
+    let nr = Number(inputNr.value);
+    let nextNr = nr + 1;
+
+return nextNr;
+
+}
