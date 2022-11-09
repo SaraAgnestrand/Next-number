ae0ea85067fd947e42fa31b1660b8955274de470
Sara Agnestrand <sara@agnestrand.net>
2022-11-09 10:17:09 +0100

    changed comments

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-
+//Deklaration of variables from html
 const inputNr=document.querySelector(".inputNr");
 const button=document.querySelector("button");
 const h3=document.querySelector("h3");
 
+
+//Adding eventlistener on button and fuction that run when click
+
 button.addEventListener("click", function() {
-    console.log("Innan funktionsanrop")
+
+    //Declaration of nextNumber
+    //Then sets innertext for h3
     const nextNumber = getNextNumber();
     h3.innerText = "Nästa nummer är " + nextNumber + ".";
     
 });
 
-
+//Function for getting nextNumber, return to function in Eventlistener
 function getNextNumber(){
     let nr = Number(inputNr.value);
     let nextNr = nr + 1;
